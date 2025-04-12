81f3d2cb9f4ee294248a95377a02b8072cfc0fd0
ibtissame2 <ibtissame.hsini@um5r.ac.ma>
2025-04-12 19:04:48 +0100

    use Router and Pinia  and install missin package

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <div class="text-red-400">Ibtissame</div>
+  <router-view />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
