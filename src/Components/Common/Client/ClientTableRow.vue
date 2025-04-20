08c8d1c00729cff446fcb8d68184d1410840ef62
ibtissame2 <ibtissame.hsini@um5r.ac.ma>
2025-04-20 18:39:27 +0100

    Convert the rest files from ts to js

--- src/Components/Common/Client/ClientTableRow.vue
+++ src/Components/Common/Client/ClientTableRow.vue
@@ -1,6 +1,4 @@
-<script setup lang="ts">
-import type { Client } from '@/packages/api/src';
-import { computed, ref } from 'vue';
+<script setup>
 import { CheckCircleIcon } from '@heroicons/vue/20/solid';
 import { useClientsStore } from '@/utils/useClients';
 import { storeToRefs } from 'pinia';
@@ -8,62 +6,65 @@ import ClientMoreOptionsDropdown from '@/Components/Common/Client/ClientMoreOpti
 import { useProjectsStore } from '@/utils/useProjects';
 import TableRow from '@/Components/TableRow.vue';
 import ClientEditModal from '@/Components/Common/Client/ClientEditModal.vue';
+import { computed, ref } from 'vue';
 
 const { projects } = storeToRefs(useProjectsStore());
 
-const props = defineProps<{
-    client: Client;
-}>();
+const props = defineProps({
+	client: {
+		type: Object,
+		required: true,
+	},
+});
 
 function deleteClient() {
-    useClientsStore().deleteClient(props.client.id);
+	useClientsStore().deleteClient(props.client.id);
 }
 
 const projectCount = computed(() => {
-    return projects.value.filter(
-        (projects) => projects.client_id === props.client.id
-    ).length;
+	return projects.value.filter((project) => project.client_id === props.client.id).length;
 });
 
 function archiveClient() {
-    useClientsStore().updateClient(props.client.id, {
-        ...props.client,
-        is_archived: !props.client.is_archived,
-    });
+	useClientsStore().updateClient(props.client.id, {
+		...props.client,
+		is_archived: !props.client.is_archived,
+	});
 }
 
 const showEditModal = ref(false);
 </script>
 
 <template>
-    <TableRow>
-        <ClientEditModal
-            v-model:show="showEditModal"
-            :client="client"></ClientEditModal>
-        <div
-            class="whitespace-nowrap flex items-center space-x-5 3xl:pl-12 py-4 pr-3 text-sm font-medium text-text-primary pl-4 sm:pl-6 lg:pl-8 3xl:pl-12">
-            <span>
-                {{ client.name }}
-            </span>
-        </div>
-        <div
-            class="whitespace-nowrap flex items-center space-x-5 3xl:pl-12 py-4 pr-3 text-sm font-medium text-text-primary pl-4 sm:pl-6 lg:pl-8 3xl:pl-12">
-            <span class="text-muted"> {{ projectCount }} Projects </span>
-        </div>
-        <div
-            class="whitespace-nowrap px-3 py-4 text-sm text-muted flex space-x-1 items-center font-medium">
-            <CheckCircleIcon class="w-5"></CheckCircleIcon>
-            <span>Active</span>
-        </div>
-        <div
-            class="relative whitespace-nowrap flex items-center pl-3 text-right text-sm font-medium sm:pr-0 pr-4 sm:pr-6 lg:pr-8 3xl:pr-12">
-            <ClientMoreOptionsDropdown
-                :client="client"
-                @edit="showEditModal = true"
-                @archive="archiveClient"
-                @delete="deleteClient"></ClientMoreOptionsDropdown>
-        </div>
-    </TableRow>
+	<TableRow>
+		<ClientEditModal v-model:show="showEditModal" :client="client" />
+		<div
+			class="whitespace-nowrap flex items-center space-x-5 3xl:pl-12 py-4 pr-3 text-sm font-medium text-text-primary pl-4 sm:pl-6 lg:pl-8 3xl:pl-12"
+		>
+			<span>
+				{{ client.name }}
+			</span>
+		</div>
+		<div
+			class="whitespace-nowrap flex items-center space-x-5 3xl:pl-12 py-4 pr-3 text-sm font-medium text-text-primary pl-4 sm:pl-6 lg:pl-8 3xl:pl-12"
+		>
+			<span class="text-muted"> {{ projectCount }} Projects </span>
+		</div>
+		<div class="whitespace-nowrap px-3 py-4 text-sm text-muted flex space-x-1 items-center font-medium">
+			<CheckCircleIcon class="w-5" />
+			<span>Active</span>
+		</div>
+		<div
+			class="relative whitespace-nowrap flex items-center pl-3 text-right text-sm font-medium sm:pr-0 pr-4 sm:pr-6 lg:pr-8 3xl:pr-12"
+		>
+			<ClientMoreOptionsDropdown
+				:client="client"
+				@edit="showEditModal = true"
+				@archive="archiveClient"
+				@delete="deleteClient"
+			/>
+		</div>
+	</TableRow>
 </template>
 
 <style scoped></style>
--- src/Components/Common/Member/MemberMultiselectDropdown.vue
+++ src/Components/Common/Member/MemberMultiselectDropdown.vue
@@ -1,29 +1,29 @@
-<script setup lang="ts">
+<script setup>
 import MultiselectDropdown from '@/packages/ui/src/Input/MultiselectDropdown.vue';
 import { useMembersStore } from '@/utils/useMembers';
 import { storeToRefs } from 'pinia';
-import type { Member } from '@/packages/api/src';
 
 const membersStore = useMembersStore();
 const { members } = storeToRefs(membersStore);
 
-function getKeyFromItem(item: Member) {
-    return item.id;
+function getKeyFromItem(item) {
+	return item.id;
 }
 
-function getNameForItem(item: Member) {
-    return item.name;
+function getNameForItem(item) {
+	return item.name;
 }
 </script>
 
 <template>
-    <MultiselectDropdown
-        search-placeholder="Search for a Member..."
-        :items="members"
-        :get-key-from-item="getKeyFromItem"
-        :get-name-for-item="getNameForItem">
-        <template #trigger>
-            <slot name="trigger"></slot>
-        </template>
-    </MultiselectDropdown>
+	<MultiselectDropdown
+		search-placeholder="Search for a Member..."
+		:items="members"
+		:get-key-from-item="getKeyFromItem"
+		:get-name-for-item="getNameForItem"
+	>
+		<template #trigger>
+			<slot name="trigger"></slot>
+		</template>
+	</MultiselectDropdown>
 </template>
