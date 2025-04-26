a4a81797a31a1dafa4794ce2653175d9588c1787
ibtissame2 <ibtissame.hsini@um5r.ac.ma>
2025-04-26 22:51:35 +0100

    Replace the entire project css assets with valid files

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
-import './assets/css/style.css';
+import './assets/css/index.css';
 
 createApp(App).use(router).use(createPinia()).mount('#app');
